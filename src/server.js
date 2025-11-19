7c0653fe5504e7d916b0f1e462ede730543bc6e3
akhilmunnuru26 <munnuruakhil26@gmail.com>
2025-11-19 14:36:07 +0530

    Debugging cors

--- src/server.js
+++ src/server.js
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 
 // Middleware
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: '*', 
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
